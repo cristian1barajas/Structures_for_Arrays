b2ec63004e0d768a8b7a2f51856df7f63b45a38d
cristian1barajas <cristian2barajas@gmail.com>
2019-04-24 16:35:17 -0500

    Informacion del Array al Neopixel
    La informacion ajustada en el Array se traslada a cada uno de los pixeles del arreglo WS2812 y se imprime dicha informacion por el monitor serial.

--- Structures_Arrays/src/main.cpp
+++ Structures_Arrays/src/main.cpp
@@ -2,7 +2,7 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 2
-#define NUMPIXELS 16
+#define NUMPIXELS 12
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -11,21 +11,42 @@ struct Array
   int Red;
   int Green;
   int Blue;
-}Pixel_Array[16];
+}Pixel_Array[12];
 
 void Set_Pixel_Array() {
   int Value_Red = 10;
-  int Value_Green = 20;
-  int Value_Blue = 30;
+  int Value_Green = 0;
+  int Value_Blue = 120;
 
-  for(int i = 0; i < 16; i++) {
+  for(int i = 0; i < 12; i++) {
     Pixel_Array[i].Red = Value_Red;
     Pixel_Array[i].Green = Value_Green;
     Pixel_Array[i].Blue = Value_Blue;
     
-    Value_Red += 1;
-    Value_Green += 5;
-    Value_Blue += 10;
+    Value_Red += 10;
+    //Value_Green += 5;
+    Value_Blue -= 10;
+  }
+}
+
+void Show_Pixel_Array() {
+  for(int i = 0; i < 12; i++) {
+    pixels.setPixelColor(i, pixels.Color(Pixel_Array[i].Red, Pixel_Array[i].Green, Pixel_Array[i].Blue));
+    pixels.show();
+  }
+}
+
+void Print_Data_Pixel_Array() {
+  Serial.println(".:Informacion del Arreglo:.");
+  for(int i = 0; i < 12; i++) {
+    Serial.print("Pixel ");
+    Serial.print(i+1);
+    Serial.print(" ");
+    Serial.print(Pixel_Array[i].Red);
+    Serial.print(" ");
+    Serial.print(Pixel_Array[i].Green);
+    Serial.print(" ");
+    Serial.println(Pixel_Array[i].Blue);
   }
 }
 
@@ -36,6 +57,8 @@ void setup() {
   pixels.show();
 
   Set_Pixel_Array();
+  Show_Pixel_Array();
+  Print_Data_Pixel_Array();
 }
 
 void loop() {
